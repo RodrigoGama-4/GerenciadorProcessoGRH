a8234bfa7de6795fe7f75a6becb9f13eee255f24
Gustavo Xavier <gustavohsx07@gmail.com>
2023-03-28 11:47:22 -0300

    Verificando campo busca e alteração de texto

--- assets/script/script_index.js
+++ assets/script/script_index.js
@@ -1,10 +1,18 @@
 var form = document.getElementById('formulario')
 var t_busca = document.getElementById('busca')
+var nome = document.getElementById('resultado_nome')
 
 form.addEventListener('submit', function(e) { 
     // alerta o valor do t_busca
-    alert(t_busca.value);
+    // alert(t_busca.value);
 
     // impede o envio do form
     e.preventDefault();
+
+    console.log(t_busca.value)
+
+    // altera texto caso não seja vazio
+    if (t_busca.value != '') {
+        nome.innerText = t_busca.value
+    }
 })
