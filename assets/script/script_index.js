11ebc370f91d80759ff377722108abd2cfaa2370
Gustavo Xavier <gustavohsx07@gmail.com>
2023-03-28 12:04:33 -0300

    Verificando busca por usuario

--- assets/script/script_index.js
+++ assets/script/script_index.js
@@ -1,6 +1,13 @@
 var form = document.getElementById('formulario')
 var t_busca = document.getElementById('busca')
 var nome = document.getElementById('resultado_nome')
+var num = document.getElementById('resultado_numero')
+var data = document.getElementById('resultado_data')
+var lista = {
+    'nome': 'Gustavo',
+    'numero_pro': '14052023',
+    'data': '10/04/2022'
+}
 
 form.addEventListener('submit', function(e) { 
     // alerta o valor do t_busca
@@ -13,6 +20,9 @@ form.addEventListener('submit', function(e) {
 
     // altera texto caso não seja vazio
     if (t_busca.value != '') {
-        nome.innerText = t_busca.value
+        if (t_busca.value == lista.nome || t_busca.value == lista.numero_pro || t_busca.value == lista.data)
+            nome.innerText = 'Nome Interessado: ' + lista.nome
+            num.innerText = 'Numero Processo: ' + lista.numero_pro
+            data.innerText = 'Data: ' + lista.data
     }
 })
