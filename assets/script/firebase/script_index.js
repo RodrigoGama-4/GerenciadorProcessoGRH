93f976ef31f5b289878afa534083482faaefa10d
None <None>
2023-04-05 10:19:53 -0300

    Implementando buscar

--- assets/script/firebase/script_index.js
+++ assets/script/firebase/script_index.js
@@ -4,68 +4,6 @@ var lista_processos = []
 var form = document.getElementById('formulario')
 var t_busca = document.getElementById('busca')
 
-const result_lista = [
-    {
-    'nome': 'Gustavo',
-    'numero_pro': '19442023',
-    'data': '10/02/2023'
-    },
-    {
-        'nome': 'Joao',
-        'numero_pro': '78232023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Maria',
-        'numero_pro': '87652023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Gustavo',
-        'numero_pro': '19442023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Joao',
-        'numero_pro': '78232023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Maria',
-        'numero_pro': '87652023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Gustavo',
-        'numero_pro': '19442023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Joao',
-        'numero_pro': '78232023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Maria',
-        'numero_pro': '87652023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Gustavo',
-        'numero_pro': '19442023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Joao',
-        'numero_pro': '78232023',
-        'data': '10/02/2023'
-    },
-    {
-        'nome': 'Maria',
-        'numero_pro': '87652023',
-        'data': '10/02/2023'
-    }
-]
 const resultado = ''
 var res = document.getElementById('resultado')
 var texto_fim = document.getElementById('fim')
@@ -74,46 +12,43 @@ texto_fim.style.visibility = 'hidden'
 
 
 form.addEventListener('submit', function(e) { 
-    // alerta o valor do t_busca
-    // alert(t_busca.value);
 
     // impede o envio do form
     e.preventDefault();
 
-    console.log(t_busca.value) 
+    console.log('Busca: ', t_busca.value)
     main();
-
-    /*
-    // altera texto caso não seja vazio
-    if (t_busca.value != '') {
-        if (t_busca.value == lista.nome || t_busca.value == lista.numero_pro || t_busca.value == lista.data)
-            nome[0].innerText = 'Nome Interessado: ' + lista.nome
-            num[0].innerText = 'Numero Processo: ' + lista.numero_pro
-            data[0].innerText = 'Data: ' + lista.data
-    }
-    */
 })
 
-function main() {
-    if (result_lista.length > 0) {
+async function main() {
+
+    if (t_busca.value.length == 0){
+        await getProcessos()
         res.style.border = '2px solid black'
         texto_fim.style.visibility = 'visible'
-        res.innerHTML = `<div class="resultado_quant"><p>Foram encontrados: ${result_lista.length} processos.</p></div>`
+        res.innerHTML = `<div class="resultado_quant"><p>Foram encontrados: ${lista_processos[0].length} processos.</p></div>`
+        construirAll();
+    } else {
+        await getProcesso(t_busca.value)
+        res.style.border = '2px solid black'
+        texto_fim.style.visibility = 'visible'
+        res.innerHTML = `<div class="resultado_quant"><p>Foram encontrados: ${lista_processos.length/2} processos.</p></div>`
         construir();
     }
+    
 }
 
 function construir() {
-    console.log(lista_processos.length)
+    console.log('Constuindo processos pesquisados, tamanho: ', lista_processos.length/2)
     console.log(lista_processos)
-    for (var i = 0; i < lista_processos.length; i++) {
+    for (var i = 1; i < lista_processos.length; i+=2) {
         
         res.innerHTML += `<div>
-            <button class="resultado_indi">
+            <button class="resultado_indi" id=${lista_processos[i-1]}>
                 <div class="resultado_indi_informacoes">
-                    <p class="resultado_nome">${lista_processos[1].interessado}</p>
-                    <p class="resultado_numero">${lista_processos[1].numeroProcesso}</p>
-                    <p class="resultado_data">${lista_processos[1].remetente}</p>
+                    <p class="resultado_nome">Interessado: ${lista_processos[i].interessado}</p>
+                    <p class="resultado_numero">Nº Processo: ${lista_processos[i].numeroProcesso}</p>
+                    <p class="resultado_data">Remetente: ${lista_processos[i].remetente}</p>
                 </div>
                 <div class="resultado_indi_icone">
                     <img src="./assets/img/seta-para-a-direita.png" alt="Icone Seta">
@@ -122,19 +57,81 @@ function construir() {
         </div>`
     } 
 
-    //identificandoDivs()
+    identificandoDivs()
 }
 
+function construirAll() {
+    console.log('Constuindo todos os processos, tamanho: ', lista_processos[0].length)
+    console.log(lista_processos)
+    for (var i = 0; i < lista_processos.length; i++) {
+        for (var j = 0; j < lista_processos[i].length; j++){
+            res.innerHTML += `<div>
+                <button class="resultado_indi" id=${lista_processos[i][j].id}>
+                    <div class="resultado_indi_informacoes">
+                        <p class="resultado_nome">Interessado: ${lista_processos[i][j].interessado}</p>
+                        <p class="resultado_numero">Nº Processo: ${lista_processos[i][j].numeroProcesso}</p>
+                        <p class="resultado_data">Remetente: ${lista_processos[i][j].remetente}</p>
+                    </div>
+                    <div class="resultado_indi_icone">
+                        <img src="./assets/img/seta-para-a-direita.png" alt="Icone Seta">
+                    </div>
+                </button>
+            </div>`
+        }
+    } 
+
+    identificandoDivs()
+}
+
+
+function identificandoDivs() {
+    var items = document.querySelectorAll('.resultado_indi')
+
+    items.forEach(function(item){
+        item.addEventListener('click', function(event){
+            var text = item.id;
+
+            console.log('Item clicado', text)
+        })
+    })
+}
 
 
 //TEMPORARIAMENTE FICA AQUI
 
 async function getProcesso(numero_processo) {
-    const q = query(collection(db, "processo"), where('numeroProcesso', '==', numero_processo)); 
+    lista_processos = []
+    const q = query(collection(db, "processo"), where('destinatario', '==', numero_processo)); 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         lista_processos.unshift(doc.id, doc.data());
     }); 
+
+    const a = query(collection(db, "processo"), where('interessado', '==', numero_processo)); 
+    const aSnapshot = await getDocs(a);
+    aSnapshot.forEach((doc) => {
+        lista_processos.unshift(doc.id, doc.data());
+    }); 
+
+    const b = query(collection(db, "processo"), where('numeroProcesso', '==', numero_processo)); 
+    const bSnapshot = await getDocs(b);
+    bSnapshot.forEach((doc) => {
+        lista_processos.unshift(doc.id, doc.data());
+    }); 
+
+    const c = query(collection(db, "processo"), where('remetente', '==', numero_processo)); 
+    const cSnapshot = await getDocs(c);
+    cSnapshot.forEach((doc) => {
+        lista_processos.unshift(doc.id, doc.data());
+    }); 
 }
 
-getProcesso("2023")
+async function getProcessos() {
+    const snapshot = await getDocs(collection(db, "processo"));
+    const processos = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    lista_processos = []
+    lista_processos.unshift(processos)
+    
+  }
+
+
