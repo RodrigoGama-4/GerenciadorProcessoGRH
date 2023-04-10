13602bf97fdb7b1fe03291874badd2c0c368a2ed
None <None>
2023-04-10 10:53:22 -0300

    Criando arquivo de estilo para a pagina de informações do processo

--- assets/script/script_processo.js
+++ assets/script/script_processo.js
@@ -11,3 +11,25 @@ function queryString() {
 }
 
 var variavel = queryString();
+
+//criar função para pesquisar pelo id no banco de dados
+
+function construir() {
+    console.log('Constuindo processos pesquisados, tamanho: ', lista_processos.length/2)
+    console.log(lista_processos)
+    for (var i = 1; i < lista_processos.length; i+=2) {
+        
+        res.innerHTML += `<div>
+            <button class="resultado_indi">
+                <div class="resultado_indi_informacoes" id=${lista_processos[i-1]}>
+                    <p class="resultado_nome">Interessado: ${lista_processos[i].interessado}</p>
+                    <p class="resultado_numero">Nº Processo: ${lista_processos[i].numeroProcesso}</p>
+                    <p class="resultado_data">Data de Entrada: ${lista_processos[i].data}</p>
+                </div>
+                <div class="resultado_indi_icone" id="${lista_processos[i-1]}excluir">
+                    <img src="./assets/img/excluir.png" alt="Icone Seta">
+                </div>
+            </button>
+        </div>`
+    } 
+}
