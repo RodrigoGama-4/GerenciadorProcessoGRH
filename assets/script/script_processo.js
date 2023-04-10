d4f57c2d4d34d4b88e94c47500d51c99813d8987
None <None>
2023-04-10 11:54:38 -0300

    Finalizando

--- assets/script/script_processo.js
+++ assets/script/script_processo.js
@@ -1,37 +1,34 @@
-import { db, addDoc, collection, getDocs, doc, query, where } from './firebase/firebase'
+import { db, addDoc, collection, getDocs, doc, query, where} from './firebase/firebase.js'
 
-lista_processos = []
+var lista_processos = []
 
-async function main() {
+main()
+
+function main() {
+    comandos()
+}
+
+async function comandos() {
     var variavel = queryString();
-    await getProcesso()
+    console.log(variavel)
     //construir()
-    console.log(lista_processos)
 }
 
 // função pra ler querystring
 function queryString() {  
     var loc = location.search.substring(1, location.search.length);   
+    var param_name;
     var param_value = false;   
     var params = loc.split("&");   
-    for (i=0; i<params.length;i++) {   
+    for (var i=0; i<params.length; i++) {   
         param_name = params[i].substring(0,params[i].indexOf('='));                                
         param_value = params[i].substring(params[i].indexOf('=')+1)
-    }   
-    alert(param_value)
+    }
     return param_value
 }
 
 //criar função para pesquisar pelo id no banco de dados
-async function getProcesso(data) {
-    lista_processos = []
-    const c = query(collection(db, "processo"), where('id', '==', data)); 
-    const cSnapshot = await getDocs(c);
-    cSnapshot.forEach((doc) => {
-        lista_processos.unshift(doc.id, doc.data());
-    }); 
-    console.log(lista_processos)
-}
+
 
 
 //implementar a função construir com base no resultado da busca no banco
