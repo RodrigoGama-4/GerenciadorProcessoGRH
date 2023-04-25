3d04aeaf4857649a1d453a57117f7da6d29f85eb
Gustavo Xavier <gustavohsx07@gmail.com>
2023-04-25 11:34:51 -0300

    Alterando dado do localstorage quando realiza logout

--- assets/script/firebase/sair.js
+++ assets/script/firebase/sair.js
@@ -3,6 +3,7 @@ import { auth } from "./firebase.js"
 let sair = document.getElementById('sair')
 
 sair.addEventListener('click', () =>{
+    localStorage.setItem('status', 'false')
     auth.signOut()
     window.location.href = "../index.html"
 })
