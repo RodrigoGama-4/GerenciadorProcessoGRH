2a8022c41f2f2c6ab0f5fb96f3675cdbd53a55d6
Gustavo Xavier <gustavohsx07@gmail.com>
2023-05-15 10:30:21 -0300

    Criando função para verificar se um processo esta dentro de um periodo de tempo

--- assets/script/script_relatorios.js
+++ assets/script/script_relatorios.js
@@ -1,11 +1,40 @@
-var estatus = localStorage.getItem('status')
+import { getProcessos } from './firebase/funcFirebase.js'
+
+document.addEventListener("DOMContentLoaded", function(){
+    ver()
+})
+
+var status = localStorage.getItem('status')
+var lista_processos = []
 
 function ver(){
-    if (estatus == "true") {
+    if (status == "true") {
         console.log("Logado")
+        main()
     } else {
         window.location = "../index.html"
     }
 }
 
-ver()
+
+async function main() {
+    lista_processos = await getProcessos()
+    console.log(lista_processos[0])
+
+    verPeriodo()
+}
+
+
+function verPeriodo() {
+    let periodoInicial;
+    let periodoFinal;
+    
+    for (var i = 0; i < lista_processos[0].length; i++) {
+        if (lista_processos[0][i].data >= "2022-04-27" && lista_processos[0][i].data <= "2023-05-28") {
+            console.log(`${lista_processos[0][i].data} Esta dentro do periodo`)
+        } else {
+            console.log(`${lista_processos[0][i].data} Não está dentro do periodo`)
+        }
+    }
+    
+}
