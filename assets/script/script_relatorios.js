49ea93ca3363fe0792e14a4b6b9a82b2b076b36d
Gustavo Xavier <gustavohsx07@gmail.com>
2023-05-15 11:20:44 -0300

    Quando clicar no botao, chamar as funcoes para verificar se esta no periodo

--- assets/script/script_relatorios.js
+++ assets/script/script_relatorios.js
@@ -7,21 +7,31 @@ document.addEventListener("DOMContentLoaded", function(){
 var status = localStorage.getItem('status')
 var lista_processos = []
 
+var form = document.getElementById('form_data')
+
 function ver(){
     if (status == "true") {
         console.log("Logado")
-        main()
     } else {
         window.location = "../index.html"
     }
 }
 
+// Fica sempre esperando que o evento de enviar (submit) occora para chamar as funções
+form.addEventListener('submit', function(e) { 
+    // impede o envio do form
+    e.preventDefault();
+
+    // Chama a função principal
+    main();
+})
+
 
 async function main() {
-    //dadosBanco()
-    //console.log(lista_processos[0])
+    await dadosBanco()
+    console.log(lista_processos[0])
 
-    //verPeriodo()
+    verPeriodo()
 }
 
 
@@ -31,15 +41,22 @@ async function dadosBanco() {
 
 
 function verPeriodo() {
-    let periodoInicial;
-    let periodoFinal;
+    let periodoInicial = document.getElementById('data_inicial')
+    let periodoFinal = document.getElementById('data_final')
+    let lista_relatorio = []
+
+    console.log(periodoInicial.value)
+    console.log(periodoFinal.value)
     
     for (var i = 0; i < lista_processos[0].length; i++) {
-        if (lista_processos[0][i].data >= "2022-04-27" && lista_processos[0][i].data <= "2023-05-28") {
+        if (lista_processos[0][i].data >= periodoInicial.value && lista_processos[0][i].data <= periodoFinal.value) {
             console.log(`${lista_processos[0][i].data} Esta dentro do periodo`)
+            lista_relatorio.unshift(lista_processos[0][i])
         } else {
             console.log(`${lista_processos[0][i].data} Não está dentro do periodo`)
         }
     }
+
+    console.log(lista_relatorio.length)
     
 }
